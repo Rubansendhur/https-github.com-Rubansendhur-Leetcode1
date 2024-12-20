7f1016ac92cdf0990d8da4f3cbff877aa8b81f48
Ruban S <rubansendhur78409@gmail.com>
2024-12-20 10:24:21 +0530

    Time: 0 ms (100%), Space: 17.9 MB (5.25%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
@@ -6,6 +6,8 @@
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
+        # approach One
+        """
         c = 0
         temp = head
 
@@ -18,7 +20,18 @@ class Solution:
         for i in range(mid):
             temp = temp.next
         
-        return temp
+        return temp"""
+
+        #approach Two using Fast and slow pointers
+
+        slow = head
+        fast = head
+
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
 
 
         
