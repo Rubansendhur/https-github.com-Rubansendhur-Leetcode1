6b5dda88f9bb80cf5eaca65e733edb6807c08619
Ruban S <rubansendhur78409@gmail.com>
2025-02-02 23:23:36 +0530

    Time: 527 ms (82.42%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,7 +1,7 @@
-SELECT id 
+SELECT distinct(w.id) 
 FROM Weather w
-WHERE temperature > (
-    SELECT temperature 
-    FROM Weather 
-    WHERE recordDate = DATE_SUB(w.recordDate, INTERVAL 1 DAY)
-);
+join weather w1 on 
+w1.recorddate = Date_sub(w.recorddate, INTERVAL 1 DAY)
+where 
+w.temperature > w1.temperature 
+order by w.id
