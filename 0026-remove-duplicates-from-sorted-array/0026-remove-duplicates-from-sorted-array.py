f396ec7741b531c901d3bf334b56fa27c231e150
Ruban S <rubansendhur78409@gmail.com>
2025-03-02 18:39:11 +0530

    Time: 0 ms (100%), Space: 18.9 MB (75.4%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,11 +4,13 @@ class Solution:
             return 0
         
         i = 0
+        c = 0
         
         for j in range(1, len(a)):
             if a[j] != a[i]:
                 i += 1
+                c += 1
                 a[i] = a[j]
         
-        return i + 1 
+        return c + 1 
         
