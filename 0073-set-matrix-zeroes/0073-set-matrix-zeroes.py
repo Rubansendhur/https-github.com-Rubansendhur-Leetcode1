92b5d13b59e1656d4b3eb9611e3e4477bc5b54d0
Ruban S <rubansendhur78409@gmail.com>
2025-07-17 23:13:48 +0530

    Time: 23 ms (5.07%), Space: 18.8 MB (9.84%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,24 +3,22 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-
         m = len(matrix)
         n = len(matrix[0])
 
-        zero_rows = set()
-        zero_cols = set()
-
+        zero_rows = []
+        zero_cols = []
+        
         # Step 1: Identify rows and columns to zero
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 0:
-                    zero_rows.add(i)
-                    zero_cols.add(j)
+                    zero_rows.append(i)
+                    zero_cols.append(j)
 
         # Step 2: Zero out identified rows and columns
         for i in range(m):
             for j in range(n):
                 if i in zero_rows or j in zero_cols:
                     matrix[i][j] = 0
-        
         
