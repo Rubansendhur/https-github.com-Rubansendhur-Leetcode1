ce37dccde0ae1ba8190d962e6e652a2371b2743c
Ruban S <rubansendhur78409@gmail.com>
2025-08-29 11:07:25 +0530

    Time: 679 ms (80.55%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT
+product_id,
+year AS first_year,
+quantity,
+price
+FROM (
+    SELECT *,
+    MIN(year) OVER (PARTITION BY product_id) AS minyear
+    FROM Sales
+) AS a
+WHERE a.year=a.minyear
